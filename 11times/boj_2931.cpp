5042a2d74568a4d2d7945b5033c7c3e207726911
LeeDongHo <edh1021@gmail.com>
2020-08-16 00:05:09 +0900

    완전탐색 + 구현

--- 11times/boj_2931.cpp
+++ 11times/boj_2931.cpp
@@ -1,103 +1,27 @@
+/*
+	[백준] 2931 가스관
+		문제 출처 : https://www.acmicpc.net/problem/2931
+
+	[풀이 방법] 구현, 완전 탐색
+		1. 유럽 전체를 순회하면서 '.'이면서 연결되는 관이 있는 경우를 찾는다.
+		2. 연결되는 관이 존재하면 벡터에 좌표기준 관의 위치를 넣는다.
+		3. 벡터의 값이 0이 아닌 경우 그에 맞는 관을 삽입해준다.
+			- 가스흐름이 유일한 경우만 입력으로 주어지므로 vec에 값이 차는 '.' 좌표는 오직 1개이다.
+
+		[실패 과정]
+			1. BFS를 이용해 풀었는데 방향에 따라 Y,X 값 구해서 이동하는 부분에서 예외처리가 틀린 부분이 있었디.
+				왠지는 더 찾아봐야겠다. -> 손절.
+
+*/
+
 #include <iostream>
 #include <vector>
 #include <queue>
 
 int R, C;
 
-char pipes[]{ '|','-','+','1','2','3','4' };
-enum direction {UP=0,DOWN,LEFT,RIGHT};
+std::vector<std::vector<char>> pipes{ {'|','+','1','4'},{ '|','+','2','3' } ,{ '-','+','1','2' } ,{ '-','+','3','4' } };
 std::pair<int, int> movePosition[]{ {-1,0},{1,0},{0,-1},{0,1} };
-std::pair<int, int> startPosition, destPosition;
-
-struct node {
-	direction dir;
-	int y;
-	int x;
-};
-
-bool isPossible(const direction nextDirection, char nextPipe)
-{
-	if (nextDirection == direction::UP)
-	{
-		if (nextPipe == pipes[3] || nextPipe == pipes[6] || nextPipe == pipes[0] || nextPipe == pipes[2])
-			return true;
-	}
-	if (nextDirection == direction::DOWN)
-	{
-		if (nextPipe == pipes[4] || nextPipe == pipes[5] || nextPipe == pipes[0] || nextPipe == pipes[2])
-			return true;
-	}
-	if (nextDirection == direction::LEFT)
-	{
-		if (nextPipe == pipes[3] || nextPipe == pipes[4] || nextPipe == pipes[1] || nextPipe == pipes[2])
-			return true;
-	}
-	if (nextDirection == direction::RIGHT)
-	{
-		if (nextPipe == pipes[5] || nextPipe == pipes[6] || nextPipe == pipes[1] || nextPipe == pipes[2])
-			return true;
-	}
-	return false;
-}
-
-direction getDirection(const direction nowDir, const char destPipe)
-{
-	if (nowDir == direction::UP)
-	{
-		if (destPipe == pipes[0])
-			return nowDir;
-		if (destPipe == pipes[2])
-			return nowDir;
-		if (destPipe == pipes[3])
-			return direction::RIGHT;
-		if (destPipe == pipes[6])
-			return direction::LEFT;
-	}
-	if (nowDir == direction::DOWN)
-	{
-		if (destPipe == pipes[0])
-			return nowDir;
-		if (destPipe == pipes[2])
-			return nowDir;
-		if (destPipe == pipes[4])
-			return direction::RIGHT;
-		if (destPipe == pipes[5])
-			return direction::LEFT;
-	}
-	if (nowDir == direction::LEFT)
-	{
-		if (destPipe == pipes[1])
-			return nowDir;
-		if (destPipe == pipes[2])
-			return nowDir;
-		if (destPipe == pipes[3])
-			return direction::DOWN;
-		if (destPipe == pipes[4])
-			return direction::UP;
-	}
-	if (nowDir == direction::RIGHT)
-	{
-		if (destPipe == pipes[1])
-			return nowDir;
-		if (destPipe == pipes[2])
-			return nowDir;
-		if (destPipe == pipes[6])
-			return direction::DOWN;
-		if (destPipe == pipes[5])
-			return direction::UP;
-	}
-	return nowDir;
-}
-
-bool isOK(const std::vector<std::vector<char>>& board, const std::vector<node>& vec)
-{
-	if (isPossible(vec[1].dir, board[vec[0].y][vec[0].x]))
-	{
-		if (isPossible(vec[2].dir, board[vec[0].y][vec[0].x]))
-			return true;
-	}
-	return false;
-}
 
 int main()
 {
@@ -106,93 +30,69 @@ int main()
 
 	std::cin >> R >> C;
 	std::vector<std::vector<char>> board(R, std::vector<char>(C));
-	std::vector<std::vector<bool>> isVisted(R, std::vector<bool>(C, false));
-	int pipeCount = 1;
+
+	for (int i = 0; i < R; i++)
+	{
+		for (int j = 0; j < C; j++)
+			std::cin >> board[i][j];
+	}
+
+	std::vector<int> vec;
 	for (int i = 0; i < R; i++)
 	{
 		for (int j = 0; j < C; j++)
 		{
-			std::cin >> board[i][j];
-			if (board[i][j] == 'M')
+			if (board[i][j] == '.')
 			{
-				startPosition = { i,j };
-				isVisted[i][j] = true;
-			}
-			else if (board[i][j] == 'Z')
-				destPosition = { i,j };
-		}
-	}
-
-	std::queue<node>  q;
-	for (int i = 0; i < 4; i++)
-	{
-		int newY = startPosition.first + movePosition[i].first;
-		int newX = startPosition.second + movePosition[i].second;
-		if (newX < 0 || newX >= C || newY < 0 || newY >= R) continue;
-		if (board[newY][newX] == '.') continue;
-		if (!isPossible((direction)i, board[newY][newX])) continue;
-		q.push({getDirection((direction)i,board[newY][newX]), newY,newX });
-		isVisted[newY][newX] = true;
-	}
-
-	while (!q.empty())
-	{
-		auto front = q.front();
-		q.pop();
-
-		if (board[front.y][front.x] == '+')
-		{
-			for (int i = 0; i < 4; i++)
-			{
-				int newY = front.y + movePosition[i].first;
-				int newX = front.x + movePosition[i].second;
-				if (isVisted[newY][newX]) continue;
-				isVisted[newY][newX] = true;
-				q.push({ getDirection(front.dir,board[newY][newX]),newY,newX });
-			}
-		}
-		else if (board[front.y][front.x] == '.')
-		{
-			std::vector<node> temp;
-			temp.push_back({ (direction)-1,front.y,front.x });
-			for (int i = 0; i < 4; i++)
-			{
-				int newY = front.y + movePosition[i].first;
-				int newX = front.x + movePosition[i].second;
-				if (newX < 0 || newX >= C || newY < 0 || newY >= R) continue;
-				if (board[newY][newX] == '.' || board[newY][newX] == 'M' || board[newY][newX] == 'Z') continue;
-				if (!isPossible((direction)i, board[newY][newX])) continue;
-				temp.push_back({ (direction)( (i%2 ==0) ? i+1 : i-1) ,newY,newX });
-			}
-			char answer;
-			if (temp.size() > 3)
-				answer = '+';
-			else
-			{
-				for (int i = 0; i < 7; i++)
+				for (int k = 0; k < 4; k++)
 				{
-					if (i == 2) continue;
-					board[front.y][front.x] = pipes[i];
-					if (isOK(board, temp))
+					int newY = i + movePosition[k].first;
+					int newX = j + movePosition[k].second;
+					if (newY < 0 || newY >= R || newX < 0 || newX >= C) continue;
+					if (board[newY][newX] == '.') continue;
+					for (int z = 0; z < 4; z++)
+						if (board[newY][newX] == pipes[k][z])
+							vec.push_back(k);
+				}
+				if (!vec.empty())
+				{
+					std::cout << i + 1 << ' ' << j + 1 << ' ';
+					if (vec.size() == 4)
 					{
-						std::cout << front.y + 1 << ' ' << front.x + 1 << ' ' << pipes[i];
+						std::cout << '+';
 						return 0;
 					}
-					board[front.y][front.x] = '.';
+					if (vec.size() == 2)
+					{
+						if (vec[0] == 0)
+						{
+							if (vec[1] == 1) std::cout << '|';
+							else if (vec[1] == 2) std::cout << '3';
+							else if (vec[1] == 3) std::cout << '2';
+						}
+						else if (vec[0] == 1)
+						{
+							if (vec[1] == 0) std::cout << '|';
+							else if (vec[1] == 2) std::cout << '4';
+							else if (vec[1] == 3) std::cout << '1';
+						}
+						else if (vec[0] == 2)
+						{
+							if (vec[1] == 0) std::cout << '3';
+							else if (vec[1] == 1) std::cout << '4';
+							else if (vec[1] == 3) std::cout << '-';
+						}
+						else if (vec[0] == 3)
+						{
+							if (vec[1] == 0) std::cout << '2';
+							else if (vec[1] == 1) std::cout << '1';
+							else if (vec[1] == 3) std::cout << '-';
+						}
+						return 0;
+					}
 				}
 			}
 		}
-		else
-		{
-			int newY = front.y + movePosition[front.dir].first;
-			int newX = front.x + movePosition[front.dir].second;
-			if (isVisted[newY][newX]) continue;
-			q.push({getDirection(front.dir,board[newY][newX]) ,newY,newX });
-			isVisted[newY][newX] = true;
-		}
-		
-
 	}
-
 	return 0;
 }
